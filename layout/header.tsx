f5f012d3504792b5e63ff14913c2ac4c3cbdd697
Amlac Eniotna <antoine.calma30@gmail.com>
2025-05-25 18:50:11 +0200

    ajout du cv

--- layout/header.tsx
+++ layout/header.tsx
@@ -1,13 +1,14 @@
-import { NotebookTabs } from "lucide-react";
+import { FileUser, NotebookTabs } from "lucide-react";
+import Link from "next/link";
 
 export const Header = () => {
   return (
-    <header className="font-editorial-old ligatures fixed z-10 w-full py-4 text-center italic backdrop-blur-sm">
+    <header className="font-editorial-old ligatures fixed z-10 w-full py-4 text-left italic backdrop-blur-sm sm:text-center">
       <div className="relative m-auto max-w-3xl px-4">
         <h1 className="text-2xl whitespace-nowrap sm:text-3xl">
           Antoine Calma
         </h1>
-        <h1 className="text-base whitespace-nowrap sm:text-xl">
+        <h1 className="text-xs whitespace-nowrap sm:text-xl">
           Développeur NextJS / UX Designer
         </h1>
         <div className="absolute top-0 flex size-full items-end-safe justify-end-safe pr-5">
@@ -15,13 +16,21 @@ export const Header = () => {
             <BookOpenText />
             <span className="text-center text-[10px]">Blog</span>
           </a> */}
-          <a
+          <Link
             href="#contact"
             className="flex cursor-pointer flex-col items-center justify-center p-2"
           >
             <NotebookTabs />
             <span className="text-center text-[10px]">Contact</span>
-          </a>
+          </Link>
+          <Link
+            href="/CV_UX_Design.pdf"
+            target="_blank"
+            className="flex cursor-pointer flex-col items-center justify-center p-2"
+          >
+            <FileUser />
+            <span className="text-center text-[10px]">Curriculum</span>
+          </Link>
         </div>
       </div>
     </header>
