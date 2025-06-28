c7dc6139a50572c4902f5b6014b2972aa16d1b7a
Amlac Eniotna <antoine.calma30@gmail.com>
2025-06-28 18:10:53 +0200

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -19,42 +19,6 @@ export default function RootLayout({
     <html lang="fr">
       <head>
         <link rel="shortcut icon" href={favicon.src} />
-
-        {/* ✅ Préchargement critique des fonts */}
-        <link
-          rel="preload"
-          href="/fonts/EditorialSans/PPEditorialSans-Ultralight.woff2"
-          as="font"
-          type="font/woff2"
-          crossOrigin="anonymous"
-        />
-        <link
-          rel="preload"
-          href="/fonts/EditorialSans/PPEditorialSans-Medium.woff2"
-          as="font"
-          type="font/woff2"
-          crossOrigin="anonymous"
-        />
-
-        {/* ✅ CSS critique inline pour éviter le FOUC */}
-        <style
-          dangerouslySetInnerHTML={{
-            __html: `
-            /* Styles critiques pour le LCP */
-            .font-editorial-sans { 
-              font-family: var(--editorial-sans), system-ui, -apple-system, sans-serif;
-            }
-            .font-editorial-old {
-              font-family: var(--editorial-old), Georgia, serif;
-            }
-            /* Optimisation pour le texte principal */
-            .lcp-text {
-              font-synthesis: none;
-              text-rendering: optimizeSpeed;
-            }
-          `,
-          }}
-        />
       </head>
       <body
         className={`${editorialOld.variable} ${editorialSans.variable} font-editorial-sans font-extralight antialiased`}
