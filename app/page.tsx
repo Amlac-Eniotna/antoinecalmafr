aad540ba70b3a098d7fa3e03a198e9467791f380
Amlac Eniotna <antoine.calma30@gmail.com>
2025-06-28 17:07:57 +0200

    test suspense

--- app/page.tsx
+++ app/page.tsx
@@ -1,13 +1,18 @@
 import { Contact } from "@/layout/contact";
 import { Me } from "@/layout/me";
 import { Projects } from "@/layout/projects";
+import { Suspense } from "react";
 
 export default function Home() {
   return (
     <main className="h-screen snap-y snap-mandatory overflow-y-auto scroll-smooth">
       <Me />
-      <Projects />
-      <Contact />
+      <Suspense>
+        <Projects />
+      </Suspense>
+      <Suspense>
+        <Contact />
+      </Suspense>
     </main>
   );
 }
